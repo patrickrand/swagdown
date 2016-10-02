288d24d8f52f88aee49045fba40d0fba22f78f5a
patrickrand <prand0001@gmail.com>
2016-10-02 17:06:00 -0400

    began adding api definitions, and swagger.io examples

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # swagdown
 Swagdown is a Golang library and CLI for generating markdown documentation from a Swagger spec. 
+
+
+### References
+ 
+ * Petstore API examples: https://github.com/OAI/OpenAPI-Specification/tree/master/examples/v2.0
---
+++ swagger/swagger.go
@@ -0,0 +1,79 @@
+package swagger
+
+import "io"
+
+func Encode(w io.Writer, api *API) error {
+	return nil
+}
+
+func Decode(r io.Reader) (*API, error) {
+
+	return nil, nil
+}
+
+type API struct {
+	Swagger  string          `json:"swagger"`
+	Info     string          `json:"info"`
+	Host     string          `json:"host"`
+	BasePath string          `json:"basePath"`
+	Schemes  []string        `json:"schemes"`
+	Consumes []string        `json:"consumes"`
+	Produces []string        `json:"produces"`
+	Paths    map[string]Path `json:"paths"`
+}
+
+type Path struct {
+	Get    *Operation `json:"get"`
+	Post   *Operation `json:"post"`
+	Put    *Operation `json:"put"`
+	Patch  *Operation `json:"patch"`
+	Delete *Operation `json:"delete"`
+	Head   *Operation `json:"head"`
+}
+
+type Operation struct {
+	Description string              `json:"description"`
+	OperationID string              `json:"operationId"`
+	Parameters  []Parameter         `json:"parameters"`
+	Responses   map[string]Response `json:"responses"`
+}
+
+type Response struct {
+	Description string `json:"description"`
+	Schema      Schema `json:"schema"`
+}
+
+type Schema struct {
+	Type  string            `json:"type"`
+	Items map[string]string `json:"items"`
+}
+
+type Parameter struct {
+	Name             string            `json:"name"`
+	In               string            `json:"in"`
+	Description      string            `json:"description"`
+	Required         bool              `json:"required`
+	Type             string            `json:"type"`
+	CollectionFormat string            `json:"collectionFormat"`
+	Items            map[string]string `json:"items"`
+}
+
+type Info struct {
+	Version        string  `json:"version"`
+	Title          string  `json:"title"`
+	Description    string  `json:"description"`
+	TermsOfService string  `json:"termsOfService"`
+	Contact        Contact `json:"contact"`
+	License        License `json:"license"`
+}
+
+type Contact struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	URL   string `json:"url"`
+}
+
+type License struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
